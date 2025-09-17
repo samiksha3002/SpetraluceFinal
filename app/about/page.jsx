cb2680621f2f8b0b868cccfb07fd9dff7c5b1bd6
Sammi_3002 <samiksha.jaiswal.bca@srwc.raisoni.net>
2025-09-17 16:09:04 +0530

    kjih

---
+++ app/about/page.jsx
@@ -0,0 +1,13 @@
+// app/about/page.jsx
+"use client";
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
+
+export default function About() {
+  return (
+    <main>
+      <Header />
+      <Footer />
+    </main>
+  );
+}
