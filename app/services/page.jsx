0d0c9658875465b17e0fe4f191de7d8031e1d596
Sammi_3002 <samiksha.jaiswal.bca@srwc.raisoni.net>
2025-09-17 15:37:39 +0530

    kjuhyg

---
+++ app/services/page.jsx
@@ -0,0 +1,12 @@
+// app/about/page.jsx
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
+
+export default function Service() {
+  return (
+    <main>
+      <Header />
+      <Footer />
+    </main>
+  );
+}
